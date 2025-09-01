cdc77b912cb0ecbffd42943d01f45fe09049f153
Chun-Yen Hsu <chunyenh@itri.org.tw>
2025-09-01 17:34:20 +0800

    WIP: add argparser.

--- auto_stash/stash_watcher.py
+++ auto_stash/stash_watcher.py
@@ -3,6 +3,7 @@ import subprocess
 import datetime
 import time
 import os
+import argparse
 
 LOG_FILE = os.path.join(os.path.dirname(__file__), "..", "logs", "auto_stash.log")
 
@@ -21,19 +22,24 @@ def has_changes(cwd, include_untracked=False) -> bool:
         cmd.append("--untracked-files=no")
 
     result = subprocess.run(
-        ["git", "status", "--porcelain", "--untracked-files=no"],
+        cmd,
         capture_output=True,
         text=True,
         cwd=cwd
     )
     return bool(result.stdout.strip())
 
-def stash_changes(cwd):
+def stash_changes(cwd, include_untracked=False):
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    cmd = ["git", "stash", "store",
+        "$(", "git", "stash", "create", ")",
+        "-m", f"auto-stash {timestamp}"]
+    
+    if include_untracked:
+        cmd.insert(2, "--include-untracked")
+        
     subprocess.run(
-        ["git", "stash", "store",
-        "$(git", "stash", "create)",
-        "-m", f"auto-stash {timestamp}"],
+        cmd,
         check=True,
         cwd=cwd
     )
@@ -54,7 +60,13 @@ def run(interval=20, cwd=None):
         time.sleep(interval)
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Git Auto Stash Watcher.")
+    parser.add_argument("-u", "--untracked", action="store_true", help="Include untracked files.")
+    parser.add_argument("-i", "--interval", type=int, default=20, help="Check interval in seconds.")
+    args = parser.parse_args()
+
     current_dir = os.path.dirname(__file__)
     parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
-    run(cwd=parent_dir)
+
+    run(interval=args.interval, include_untracked=args.untracked, cwd=parent_dir)
 
